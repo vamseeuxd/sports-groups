234f5a4f7304cca0a1e9c2326108e1092da7de39
vamseeuxd <vamsee.uxd@gmail.com>
2025-10-22 07:35:11 +0530

    feat: add landing page and manage group components with routing
    - Implemented routing for landing page and manage group components in app.routes.ts
    - Created landing page component with HTML and SCSS files
    - Added manage group component with placeholder HTML and SCSS files
    - Updated index.html to include manifest and Google Fonts
    - Enhanced global styles in styles.scss for Angular Material theming

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./landing-page/landing-page').then(c => c.LandingPage)
+    },
+    {
+        path: 'groups',
+        loadComponent: () => import('./manage-group/manage-group').then(c => c.ManageGroup)
+    },
+];
---
+++ src/app/manage-group/manage-group.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-group',
+  imports: [],
+  templateUrl: './manage-group.html',
+  styleUrl: './manage-group.scss'
+})
+export class ManageGroup {
+
+}
